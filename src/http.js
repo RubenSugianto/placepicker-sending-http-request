19febca77fc8f26f7e172d369d50cc6993e8a0b4
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-17 22:45:57 +0700

    Sending Data with POST Requests

--- src/http.js
+++ src/http.js
@@ -8,3 +8,22 @@ export async function fetchAvailablePlaces() {
 
     return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        // ini {places: places} harusnya tapi bisa ditulis {places}
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+
+    if(!response.ok) {
+        throw new Error('Failed to update user data.');
+    }
+
+    return resData.message;
+}
